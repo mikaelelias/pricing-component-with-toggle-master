4872ea0452a3f0db294ea35b7926b52c50136336
Mikael Elias <129218135+mikaelelias@users.noreply.github.com>
2023-05-02 07:23:47 -0300

    JavaScript funcion

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-const botao = document.querySelector('.checkbox');
+const botao = document.getElementById('checkbox');
     botao.addEventListener('click', function() {
-    document.querySelector(".txt_2").innerHTML = "test"         
+    document.getElementById("card_1_text2").innerHTML = "&dollar;199.99"    
+    document.getElementById("card_2_text2").innerHTML = "&dollar;299.99"      
+    document.getElementById("card_3_text2").innerHTML = "&dollar;399.99"          
 });
-
-
-  
