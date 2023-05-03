9bdfc1412f1e5586194258c162c1902de91c86b3
Mikael Elias <129218135+mikaelelias@users.noreply.github.com>
2023-05-02 21:30:57 -0300

    progress

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
+let ativado = false;
 const botao = document.getElementById('checkbox');
-    botao.addEventListener('click', function() {
-    document.getElementById("card_1_text2").innerHTML = "&dollar;199.99"    
-    document.getElementById("card_2_text2").innerHTML = "&dollar;299.99"      
-    document.getElementById("card_3_text2").innerHTML = "&dollar;399.99"          
+
+botao.addEventListener('click', function() {
+  if (!ativado) {
+    document.getElementById("card_1_text2").innerHTML = "&dollar;199.99";
+    document.getElementById("card_2_text2").innerHTML = "&dollar;299.99";
+    document.getElementById("card_3_text2").innerHTML = "&dollar;399.99";
+    ativado = true;
+  } else {
+    document.getElementById("card_1_text2").innerHTML = "&dollar;19.99";
+    document.getElementById("card_2_text2").innerHTML = "&dollar;29.99";
+    document.getElementById("card_3_text2").innerHTML = "&dollar;39.99";
+    ativado = false;
+  }
 });
